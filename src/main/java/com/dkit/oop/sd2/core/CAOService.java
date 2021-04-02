c12685d9b22847e3479f269989dee2ddcde05f76
unknown <denniswillie2000@gmail.com>
2021-04-02 14:03:58 +0100

    add cao service

---
+++ src/main/java/com/dkit/oop/sd2/core/CAOService.java
@@ -0,0 +1,25 @@
+package com.dkit.oop.sd2.core;
+
+public class CAOService {
+    public static final int PORT_NUM = 50025;
+    public static final String HOSTNAME = "localhost";
+    public static final String BREAKING_CHARACTER = "%%";
+    public static final String VARIABLE = "$";
+    public static final String REGISTER = "register";
+    public static final String REGISTERED = "registered";
+    public static final String LOGIN = "login";
+    public static final String LOGGED_IN = "logged in";
+    public static final String LOGOUT = "logout";
+    public static final String LOGGED_OUT = "logged out";
+    public static final String DISPLAY_COURSE = "display course";
+    public static final String DISPLAY_FAILED = "display failed";
+    public static final String DISPLAY_ALL = "display all";
+    public static final String FAILED_DISPLAY_ALL = "failed display all";
+    public static final String SUCCESSFUL_DISPLAY = "successful display";
+    public static final String DISPLAY_CURRENT = "display current";
+    public static final String FAILED_DISPLAY_CURRENT = "failed display current";
+    public static final String SUCCESSFUL_DISPLAY_CURRENT = "successful display current";
+    public static final String UPDATE_CURRENT = "update current";
+    public static final String FAILED_UPDATE_CURRENT = "failed update current";
+    public static final String SUCCESSFUL_UPDATE_CURRENT = "successful update current";
+}
